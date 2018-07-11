d731036a20f34e8250fb0310ae086cbe82a1ba85
zoucao <zoucao.zc@alibaba-inc.com>
2018-07-11 16:43:04 +0800

    update

---
+++ driver_module/kmemcache/kmemcache_entry.c
@@ -0,0 +1,43 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/err.h>
+#include <linux/clk.h>
+#include <linux/io.h>
+#include <linux/device.h>
+#include <linux/gpio.h>
+#include <linux/slab.h>
+#include <linux/mm.h>
+#include <linux/interrupt.h>
+#include <linux/syscore_ops.h>
+
+#define NEW_CACHE_NAME "metest64"
+
+struct kmem_cache *new_kmem_cache;
+static void *addr=NULL;
+
+static int __init kmemcachedriver_init(void)
+{
+	new_kmem_cache = kmem_cache_create(NEW_CACHE_NAME, 93, 0,
+						   SLAB_HWCACHE_ALIGN | SLAB_PANIC | SLAB_NOTRACK ,
+						   NULL);
+
+	printk("zz %s new_kmem_cache:%lx \n",__func__, (long int)new_kmem_cache);
+
+	addr = kmem_cache_alloc(new_kmem_cache, GFP_KERNEL);
+	printk("zz %s addr:%lx \n",__func__, (long int)addr);
+		
+	printk("kmemcachedriver load \n");
+	return 0;
+}
+
+static void __exit kmemcachedriver_exit(void)
+{
+	kmem_cache_destroy(new_kmem_cache);
+	printk("kmemcachedriver unload \n");
+}
+
+module_init(kmemcachedriver_init);
+module_exit(kmemcachedriver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Zou Cao<zoucaox@outlook.com>");
