76da29d40caec743f22b331c4f5a319349cffb5d
zoucao <zoucaox@outlook.com>
2018-10-15 14:05:39 +0800

    add pgd pud pmd pte

--- driver_module/page_info/page_entry.c
+++ driver_module/page_info/page_entry.c
@@ -10,7 +10,52 @@
 #include <linux/interrupt.h>
 #include <linux/syscore_ops.h>
 #include <linux/mmzone.h>
+#include <linux/init.h>
+#include <linux/pid.h>
+#include <linux/sched.h>
+#include <asm/current.h>
+#include <asm/futex.h>
 
+
+static pte_t *pgd_pud_pnd_pte_dump(unsigned long addr)
+{
+	struct mm_struct *mm = current->mm;
+	unsigned long *p_buf;
+
+	pgd_t *pgd;
+	pud_t *pud;
+	pmd_t *pmd = NULL;
+	pte_t *pte = NULL;
+
+	printk("zz %s mm->pgd:%lx \n",__func__, (unsigned long)mm->pgd);
+	printk("zz %s addr:%lx \n",__func__, (unsigned long)addr);
+	pgd = pgd_offset(mm, addr);
+	if (pgd_present(*pgd)) {
+		pud = pud_offset(pgd, addr);
+		if (pud_present(*pud)) {
+			if (pud_large(*pud))
+				return (pte_t *)pud;
+			pmd = pmd_offset(pud, addr);
+
+			if (pmd_present(*pmd)) {
+				pte = pte_offset_kernel(pmd, addr);
+				if (pte_present(*pte)) {
+					printk("zz %s pgd:%lx pud:%lx pmd:%lx pte:%lx \n",__func__, (unsigned long)pgd, (unsigned long)pud, (unsigned long)pmd, (unsigned long)pte);
+					printk("zz %s pgd:%lx pud:%lx pmd:%lx pte:%lx \n",__func__, (unsigned long)pgd_index(addr), (unsigned long)pud_index(addr), (unsigned long)pmd_index(addr), (unsigned long)pte_index(addr));
+					p_buf= (unsigned long*)pgd;
+					printk("zz %s pgd:%lx \n",__func__, (unsigned long)*p_buf);
+					p_buf= (unsigned long*)pud;
+					printk("zz %s pud:%lx \n",__func__, (unsigned long)*p_buf);
+					p_buf= (unsigned long*)pmd;
+					printk("zz %s pmd:%lx \n",__func__, (unsigned long)*p_buf);
+					printk("zz %s pte:%lx \n",__func__, (unsigned long)pte);
+					return pte;
+				}
+			}
+		}
+	}
+	return pte;
+}
 
 static int __init pagedriver_init(void)
 {
@@ -21,9 +66,15 @@ static int __init pagedriver_init(void)
 	page=virt_to_page(addr);
 	pfn=page_to_pfn(page);
 	printk("page:%lx addr:%lx pfn:%lx\n", page, addr, pfn);
-	printk("page:%lx addr:%lx pfn:%lx\n", page, addr, PHYS_PFN_OFFSET);
+	printk("page:%lx addr:%lx pfn:%lx\n", page, addr, 0);
 	kfree(addr);
+
+	addr = (void *) vmalloc((unsigned int) 4096);
+	printk("addr vmalloc:%lx\n", addr);
+	vfree(addr);
+
 	printk("pagedriver load \n");
+	pgd_pud_pnd_pte_dump(&nr_cpu_ids);
 	return 0;
 }
 
