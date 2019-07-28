9dad110c18b3b1cafd4526e4c2f782594dd7280d
zou cao <zoucaox@outlook.com>
2019-07-28 14:20:51 +0800

    update

--- driver_module/misc/misc_template/misc_entry.c
+++ driver_module/misc/misc_template/misc_entry.c
@@ -78,14 +78,69 @@ static int misc_template_release (struct inode *inode, struct file *file)
 	return 0;
 }
 
+static pte_t *pgd_pud_pnd_pte_dump(unsigned long addr)
+{
+	struct mm_struct *mm = current->mm;
+	unsigned long *p_buf;
+
+	struct page *page;
+	pgd_t *pgd;
+	pud_t *pud;
+	pmd_t *pmd = NULL;
+	pte_t *pte = NULL;
+
+	printk("zz %s mm->pgd:%lx \n",__func__, (unsigned long)mm->pgd);
+	printk("zz %s addr:%lx \n",__func__, (unsigned long)addr);
+	page=virt_to_page(addr);
+	pgd = pgd_offset(mm, addr);
+	if (pgd_present(*pgd)) {
+		pud = pud_offset(pgd, addr);
+		if (pud_present(*pud)) {
+			if (pud_large(*pud))
+				return (pte_t *)pud;
+			pmd = pmd_offset(pud, addr);
+
+			if (pmd_present(*pmd)) {
+				pte = pte_offset_kernel(pmd, addr);
+				if (pte_present(*pte)) {
+					printk("zz %s page:%lx \n",__func__, (unsigned long)page);
+					printk("zz %s pgd:%lx pud:%lx pmd:%lx pte:%lx \n",__func__, (unsigned long)pgd, (unsigned long)pud, (unsigned long)pmd, (unsigned long)pte);
+					printk("zz %s pgd:%lx pud:%lx pmd:%lx pte:%lx \n",__func__, (unsigned long)pgd_index(addr), (unsigned long)pud_index(addr), (unsigned long)pmd_index(addr), (unsigned long)pte_index(addr));
+					p_buf= (unsigned long*)pgd;
+					printk("zz %s pgd:%lx \n",__func__, (unsigned long)*p_buf);
+					p_buf= (unsigned long*)pud;
+					printk("zz %s pud:%lx \n",__func__, (unsigned long)*p_buf);
+					p_buf= (unsigned long*)pmd;
+					printk("zz %s pmd:%lx \n",__func__, (unsigned long)*p_buf);
+
+					printk("zz %s pte:%lx val:%lx phy:%lx addrphy:%lx\n",__func__, (unsigned long)pte, (unsigned long)*((unsigned long *)pte),(unsigned long)virt_to_phys(pte), (unsigned long)virt_to_phys(addr));
+					return pte;
+				}
+			}
+		}
+	}
+	return pte;
+}
 static void test_user_page(u64 addr)
 {
 	struct page **pages;
 	struct page *page;
+	void *data;
+	u64 *p;
+
+	p = (u64 *)addr;
+	
+	data =kmalloc(4096, GFP_KERNEL);
+	
+	pgd_pud_pnd_pte_dump(data);
+	printk("zz %s data:%lx page:%lx\n",__func__, (unsigned long)data, (unsigned long)virt_to_page(data));
 	//alloc_page();
 	printk("zz %s addr:%lx \n",__func__, (unsigned long)addr);
 	pages = kcalloc(1, sizeof(struct page *), GFP_KERNEL);
 	get_user_pages_fast(addr, 1, 1, &pages[0]);
+	pgd_pud_pnd_pte_dump((u64)page_to_virt(pages[0]));
+	printk("zz %s val:%lx \n",__func__, (unsigned long)p[0]);
+
 }
 
 static long misc_template_unlocked_ioctl (struct file *file, unsigned int cmd, unsigned long arg)
@@ -104,9 +159,11 @@ static long misc_template_unlocked_ioctl (struct file *file, unsigned int cmd, u
 	}
 	printk("zz %s cmd:%lx arg:%lx \n",__func__, (unsigned long)cmd, (unsigned long)arg);
 #else
+	printk("cmd:%d arg:%lx\n", cmd, arg);
 	switch (cmd) {
 		case IOCMD_USERMAP:
-			test_user_page((u64)cmd);
+			pgd_pud_pnd_pte_dump(arg);
+			test_user_page((u64)arg);
 			break;
 		default:
 			goto OUT;
