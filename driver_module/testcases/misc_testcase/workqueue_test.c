3cdc3b1526543bbbfa738514f1100dbe92bb0df1
zoucaox <zoucaox@outlook.com>
2020-07-25 16:39:19 +0800

    update

--- driver_module/testcases/misc_testcase/workqueue_test.c
+++ driver_module/testcases/misc_testcase/workqueue_test.c
@@ -30,7 +30,15 @@ struct wkq_data {
 	struct work_struct wq_sigtestwq_spinlock;
 	struct work_struct wq_sigtestwq_spinlockirq;
 	struct work_struct wq_sigtestwq_spinlockirq_race;
+	struct work_struct wq_sigtestwq_performace_delay;
+
 	struct delayed_work wkq_delay_test;
+
+	wait_queue_head_t wait;
+	u8 wq_complete;
+	unsigned long performance_delay;
+	unsigned long performance_exp;
+
 	int runtime;
 } *wkq_dt;
 
@@ -91,6 +99,16 @@ static void wkq_sig_spinlockirq_race_test(struct work_struct *work)
 	spin_unlock_irqrestore(&locktest_lock, locktest_irqlock_flags);
 }
 
+//caculte the delay time from queuework to run the workqueue function
+static void wkq_sig_performace_delay(struct work_struct *work)
+{
+	struct wkq_data *wkq_dt = container_of(work, struct wkq_data, wq_sigtestwq_performace_delay);
+	unsigned long tsc_val = rdtsc();
+	wkq_dt->performance_exp = tsc_val - wkq_dt->performance_delay;
+	wkq_dt->wq_complete = 1;
+	wake_up(&wkq_dt->wait);
+}
+
 int workqueue_ioctl_func(unsigned int  cmd, unsigned long addr, struct ioctl_data *data)
 {
 
@@ -115,6 +133,21 @@ int workqueue_ioctl_func(unsigned int  cmd, unsigned long addr, struct ioctl_dat
 			queue_work(wkq_dt->workqueue, &wkq_dt->wq_sigtestwq_spinlockirq);
 			queue_work(wkq_dt->sigtestworkqueue, &wkq_dt->wq_sigtestwq_spinlockirq);
 			DEBUG("workqueue sig spinlock irq\n");
+			break;
+
+		case  IOCTL_USEWORKQUEUE_PEFORMANCE_DELAY:
+			queue_work(wkq_dt->sigtestworkqueue, &wkq_dt->wq_sigtestwq_performace_delay);
+			init_waitqueue_head(&wkq_dt->wait);
+#ifdef CONFIG_X86
+			wkq_dt->performance_delay = rdtsc();
+			wkq_dt->wq_complete = 0;
+			if (!wait_event_interruptible_timeout(wkq_dt->wait, wkq_dt->wq_complete, 100*HZ))
+				ret = -EBUSY;
+			else 
+				copy_to_user((char __user *) data->wq_data.workqueue_performance,  &wkq_dt->performance_exp,
+						8);
+#endif
+			DEBUG("workqueue performace test\n");
 			break;
 		default:
 			goto OUT;
@@ -145,6 +178,7 @@ int workqueue_test_init(void)
 	INIT_WORK(&wkq_dt->wq_sigtestwq_spinlock, wkq_sig_spinlock_test);
 	INIT_WORK(&wkq_dt->wq_sigtestwq_spinlockirq, wkq_sig_spinlockirq_test);
 	INIT_WORK(&wkq_dt->wq_sigtestwq_spinlockirq_race, wkq_sig_spinlockirq_race_test);
+	INIT_WORK(&wkq_dt->wq_sigtestwq_performace_delay, wkq_sig_performace_delay);
 	INIT_DELAYED_WORK(&wkq_dt->wkq_delay_test, wkq_delay_test);
 
 	return 0;
