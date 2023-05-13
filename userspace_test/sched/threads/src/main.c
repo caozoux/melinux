c46c35fd7b39dfcdbeb134f0a23f46ea0a4bd9fc
zou cao <zoucaox@outlook.com>
2023-05-13 11:41:16 +0800

    update

--- userspace_test/sched/threads/src/main.c
+++ userspace_test/sched/threads/src/main.c
@@ -5,17 +5,19 @@
 #include<unistd.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<pthread.h>
 #include<sys/time.h>
 #include<semaphore.h>
-#include<sched.h>
+#include<linux/sched.h>
 
 #define MAX_TRHEAD  (128)
 
-static unsigned long run_val = 1UL<<20;
 unsigned long run_cnt;
-unsigned long sleep_cnt;
+unsigned long sleep_cnt=0;
 unsigned long thread_cnt;
 unsigned long loop_cnt;
+unsigned long msec_cnt;
+int sched_policy = 0;
 
 struct thread_data {
 	unsigned long id;
@@ -31,20 +33,33 @@ void *threadFunc(void *param)
 	struct timezone tz;
 	unsigned long val = 0;
 	struct thread_data *data = (struct thread_data *)param;
-	int local_run = run_cnt;
+	int policy = SCHED_NORMAL;
+	struct sched_param my_params;
+	int rc, local_loop_cnt = loop_cnt;
+
     gettimeofday (&tvpre , &tz);
-	while(local_run-->0) {
-		int local_loop_cnt = loop_cnt;
+	//my_params.sched_priority=sched_get_priority_max(sched_policy);// 尽可能高的实时优先级
+	my_params.sched_priority=sched_get_priority_min(sched_policy);// 尽可能高的实时优先级
 
-		while(local_loop_cnt-->0) {
-			val = 0;
-			while(val++<run_val);
+	if (sched_policy) {
+		//rc = sched_setscheduler(0,sched_policy, &my_params);
+		rc = sched_setscheduler(0,sched_policy, &my_params);
+		printf("set sched policy:%d priority:%d\n", sched_policy, my_params.sched_priority);
+		if(rc<0)
+		{
+			//perror("sched_setscheduler to %d error", sched_policy);
+			perror("sched_setscheduler error");
+			exit(0);
 		}
+	}
 
+	while(local_loop_cnt-->0) {
+		val = 0;
+		while(val++<msec_cnt);
 		if (sleep_cnt)
 			usleep(sleep_cnt*1000);
-
 	}
+
     gettimeofday (&tvafter , &tz);
 	data->run_val_ms = (tvafter.tv_sec-tvpre.tv_sec)*1000+(tvafter.tv_usec-tvpre.tv_usec)/1000;
     printf("花费时间:%ld ms\n", (tvafter.tv_sec-tvpre.tv_sec)*1000+(tvafter.tv_usec-tvpre.tv_usec)/1000);
@@ -56,14 +71,31 @@ void help(void)
 	printf("--run_cnt   run runtime count\n");
 	printf("--loop_cnt  run loop runtime count\n");
 	printf("--sleep_cnt sleep run count\n");
-	printf("--sched_policy sleep run count\n");
-	printf("--sched_priority sleep run count\n");
+	printf("--sched_policy fifo/deadline/cpuidle/qos\n");
+	printf("--sched_priority  sched priority vaule -20 - 120\n");
+}
+
+static int check_cpu_runtime(void)
+{
+	struct timeval tvafter,tvpre;
+	struct timezone tz;
+	unsigned long usec;
+
+	unsigned long cnt = 1UL<<30;
+    gettimeofday (&tvpre , &tz);
+
+	while(cnt-->0);
+    gettimeofday (&tvafter , &tz);
+
+	usec= (tvafter.tv_sec-tvpre.tv_sec)*1000+(tvafter.tv_usec-tvpre.tv_usec)/1000;
+	msec_cnt = (1UL<<30)/usec;
+    printf("花费时间:%ld ms %ld\n", usec, msec_cnt);
 }
 
 int main(int argc, char *argv[])
 {
 	int choice;
-	int i, sched_priority = -1, sched_policy = -1;
+	int i, sched_priority = -1;
 	struct sched_param param;
 	int maxpri;
 
@@ -76,6 +108,7 @@ int main(int argc, char *argv[])
 		help();
 		return 0;
 	}
+
 
 	while (1)
 	{
@@ -96,7 +129,7 @@ int main(int argc, char *argv[])
 		};
 	
 		int option_index = 0;
-		choice = getopt_long( argc, argv, "vht:r:s:l:",
+		choice = getopt_long(argc, argv, "vht:r:s:l:p:",
 					long_options, &option_index);
 	
 		if (choice == -1)
@@ -127,21 +160,22 @@ int main(int argc, char *argv[])
 				sleep_cnt = atoi(optarg);
 				break;
 	
-			case '?':
-				/* getopt_long will have already printed an error */
-				break;
-	
 			case 'p':
 				if (strstr("fifo", optarg))
-					sched_priority = SCHED_FIFO;
+					sched_policy = SCHED_FIFO;
 				else if (strstr("deadline", optarg))
-					//sched_priority = SCHED_DEADLINE;
-					sched_priority = 6;
+					sched_priority = SCHED_DEADLINE;
 				else if (strstr("cpuidle", optarg))
 					//sched_priority = SCHED_IDLE;
-					sched_priority = 5;
+					sched_policy = SCHED_IDLE;
+				else if (strstr("rr", optarg))
+   					sched_policy = SCHED_RR;
 				else if (strstr("qos", optarg))
 					sched_priority = 7;
+				else {
+					printf("sched policy not support\n");
+					exit(1);
+				}
 				/* getopt_long will have already printed an error */
 				break;
 
@@ -154,6 +188,7 @@ int main(int argc, char *argv[])
 		}
 	}
 	
+	check_cpu_runtime();
 	for (i = 0; i < thread_cnt; ++i) {
 		thread_data_array[i].id = i;
 		thread_data_array[i].run_val_ms = 0;
@@ -166,3 +201,4 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
