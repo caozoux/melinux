2d9d5f094e1d49c932ba4197ae16d74acb5a6d1d
Zou Cao <zoucaox@outlook.com>
2023-11-19 07:47:47 -0500

    update

--- userspace_test/memory/src/main.cc
+++ userspace_test/memory/src/main.cc
@@ -21,6 +21,7 @@ static void help()
 	printf("-t|--type:  anon/dirty/shem/mmap/thp\n");	
 	printf("-f|--file:  specify file name\n");	
 	printf("-s|--size:  page count\n");	
+	printf("-k|--fork:  fork proccessor\n");	
 }
 
 static struct option long_options[] =
@@ -30,20 +31,18 @@ static struct option long_options[] =
 	/* Argument styles: no_argument, required_argument, optional_argument */
 	{"version", no_argument,	0,	'v'},
 	{"help",	no_argument,	0,	'h'},
+	{"fork",	no_argument,	0,	'k'},
 	{"type",	required_argument,	0,	't'},
 	{"file",required_argument,	0,	'f'},
 	{"size",required_argument,	0,	's'},
 	{0,0,0,0}
 };
 
-void ()
-{
-	
-}
 
 int main(int argc, char *argv[])
 {
 	int args_size = 0;
+	int args_fork = 0;
 	char args_type[64] = {0};
 	char args_file[256] = {0};
 	char pagebuf[4096] = {0};
@@ -52,7 +51,7 @@ int main(int argc, char *argv[])
 
 	while (1) {
 		int option_index = 0;
-		choice = getopt_long( argc, argv, "vht:-f:s:",
+		choice = getopt_long( argc, argv, "vht:-f:s:k",
 					long_options, &option_index);
 		if (choice == -1)
 			break;
@@ -75,6 +74,10 @@ int main(int argc, char *argv[])
 
 			case 's':
 				args_size = atoi(optarg);
+				break;
+
+			case 'k':
+				args_fork= 1;
 				break;
 
 			default:
@@ -105,10 +108,10 @@ int main(int argc, char *argv[])
 		buf = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 		if (buf == MAP_FAILED)
 			exit(1);
-		ret = madvise(buf, size, MADV_HUGEPAGE);
+		//ret = madvise(buf, size, MADV_HUGEPAGE);
 		if (ret)
 			exit(1);
-		memset(buf, 0, size);
+		//memset(buf, 0, size);
 	} else if (!strcmp(args_type, "mmap")) {
 		unsigned long cnt = args_size;
 		char  *addr;
@@ -117,17 +120,29 @@ int main(int argc, char *argv[])
 			printf("Err: open %s failed\n", args_file);
 			goto out;
 		}
-		addr = (char *)mmap(0, MAP_LENGTH, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		addr = (char *)mmap(0, cnt, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 		printf("zz %s addr:%lx cnt:%lx \n",__func__, (unsigned long)addr, (unsigned long)cnt);
 		//while(cnt--) {
-			memset(addr, 0, 4096);
-			addr += 4096;
+		//	memset(addr, 0, 4096);
+		//	addr += 4096;
 		//}
 	} else {
 			printf("Err: type is lost\n");
 			goto out;
 	}
 
+
+	if (args_fork) {
+				
+		if(fork()) // 父进程
+		{ 
+			while(1)
+				sleep(1);
+		} else { // 子进程读
+			while(1)
+				sleep(1);
+		}
+	}
 	while(1)
 		sleep(1);
 	return 0;
