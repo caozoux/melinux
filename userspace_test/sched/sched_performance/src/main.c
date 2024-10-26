9ce948ffadc2115ee8489a8de6383a5d43e7bdc8
Zou Cao <zoucaox@gmail.com>
2024-10-26 09:14:08 +0800

    update

---
+++ userspace_test/sched/sched_performance/src/main.c
@@ -0,0 +1,289 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<getopt.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/time.h>
+#include<signal.h>
+#include<semaphore.h>
+#include<linux/sched.h>
+
+#define MAX_TRHEAD  (128)
+
+static unsigned long run_val = 1UL<<20;
+unsigned long args_thread_cnt = 0;
+int arg_cpu_util;
+
+struct thread_data {
+	unsigned long id;
+	unsigned long cpu_util;
+};
+enum test_case {
+	CASE_NULL,
+	CASE_BALANCE,
+};
+
+pthread_t thread_array[MAX_TRHEAD];
+struct thread_data thread_data_array[MAX_TRHEAD];
+static unsigned long cpu_10_ms_count;
+static unsigned long cpu_1_ms_count;
+static int args_sched_policy = 0;
+static int args_sched_priority = -1;
+static int args_util_increase = 0;
+static int args_report = 0;
+static enum test_case args_test_case = CASE_NULL;
+
+static unsigned long *report_data;
+
+static void cpu_10_ms_time(void)
+{
+	unsigned long cnt = cpu_10_ms_count;
+	while(cnt--);
+}
+
+static void cpu_1_ms_time(void)
+{
+	unsigned long cnt = cpu_1_ms_count;
+	while(cnt--);
+}
+
+void change_sched_policy(void)
+{
+	struct sched_param my_params;
+	int policy = SCHED_NORMAL;
+	int rc;
+
+	if (args_sched_policy) {
+		rc = sched_setscheduler(0, args_sched_policy, &my_params);
+		printf("set sched policy:%d priority:%d\n", args_sched_policy, my_params.sched_priority);
+		if(rc<0)
+		{
+			//perror("sched_setscheduler to %d error", args_sched_policy);
+			perror("sched_setscheduler error");
+			exit(0);
+		}
+	}
+}
+
+void *threadFunc(void *param)
+{
+	struct thread_data *data = (struct thread_data *)param;
+	int i, j;
+	int cpu_util = data->cpu_util;
+	int sleep_ms = 1000 - arg_cpu_util * 10;
+
+	while(1) {
+		j = 0;
+		for (i = 0; i < cpu_util; ++i) {
+			for (j = 0; j < 10; j++) {
+				cpu_1_ms_time();
+				report_data[data->id] += 1;
+			}
+		}
+		usleep(sleep_ms*1000);
+	}
+}
+
+void test_case_balance()
+{
+	int i, cnt = 5;
+	arg_cpu_util = 100;
+
+	for (i = 0; i < cnt; ++i) {
+		thread_data_array[i].id = i;
+		thread_data_array[i].cpu_util= 0;
+		pthread_create(&thread_array[i], NULL, threadFunc, &thread_data_array[i]);
+		sleep(5);
+	}
+
+	for (i = 0; i < cnt; ++i) {
+		pthread_join(thread_array[i], NULL);
+	}
+}
+
+void run_test_cases(enum test_case case_item)
+{
+	switch (case_item) {
+		case CASE_BALANCE:
+			test_case_balance();
+			break;
+		default:
+			break;
+	}
+}
+
+void help(void)
+{
+	printf("-t/--threads    run thread numbers\n");
+	printf("-u/--cpu_util   per thread cpu util(1-100)\n");
+	printf("-p/--sched_policy fifo/deadline/cpuidle/qos\n");
+	printf("-c/--case         balance\n");
+}
+
+static unsigned long old_data = 0;
+
+void handle_alarm(int sig) 
+{
+	int i;
+	unsigned long new_data = 0;
+
+	for (i = 0; i < args_thread_cnt; ++i) {
+		new_data += report_data[i];
+	}
+	
+	printf("tick:%ld\n", (new_data - old_data));
+	old_data = new_data;
+	if (args_report)
+		alarm(1);
+}
+
+static void caculte_cpu_10_counts(void)
+{
+	struct timeval tvafter,tvpre;
+	unsigned long ts;
+	unsigned long cnt = 1UL<<24;
+
+    gettimeofday (&tvpre , NULL);
+
+	while(cnt--);
+
+    gettimeofday (&tvafter, NULL);
+	ts = (tvafter.tv_sec-tvpre.tv_sec)*1000+(tvafter.tv_usec-tvpre.tv_usec)/1000;
+	cpu_10_ms_count = ((1UL<<24)/ts)*10;
+	cpu_1_ms_count = cpu_10_ms_count/10;
+}
+
+int main(int argc, char *argv[])
+{
+	int choice;
+	int i;
+
+	args_thread_cnt = 0;
+
+	caculte_cpu_10_counts();
+	if (argc < 2) {
+		help();
+		return 0;
+	}
+
+	while (1)
+	{
+		static struct option long_options[] =
+		{
+			/* Use flags like so:
+			{"verbose",	no_argument,	&verbose_flag, 'V'}*/
+			/* Argument styles: no_argument, required_argument, optional_argument */
+			{"version", no_argument,	0,	'v'},
+			{"help",	no_argument,	0,	'h'},
+			{"threads",	required_argument,	0,	't'},
+			{"cpu_util",required_argument,	0,	'u'},
+			{"report", no_argument,	0,	'r'},
+			{"sched_policy", required_argument,	0,	'p'},
+			{"cases", required_argument,	0,	'c'},
+			{"util_increase", required_argument,	0,	'e'},
+			{0,0,0,0}
+		};
+	
+		int option_index = 0;
+		choice = getopt_long( argc, argv, "vht:u:p:c:re:", long_options, &option_index);
+	
+		if (choice == -1)
+			break;
+	
+		switch( choice )
+		{
+			case 'v':
+				break;
+	
+			case 'h':
+				help();
+				return 0;
+	
+			case 't':
+				args_thread_cnt = atoi(optarg);
+				break;
+
+			case 'p':
+				if (strstr("fifo", optarg))
+					args_sched_policy = SCHED_FIFO;
+				else if (strstr("deadline", optarg))
+					args_sched_priority = SCHED_DEADLINE;
+				else if (strstr("cpuidle", optarg))
+					//sched_priority = SCHED_IDLE;
+					args_sched_policy = SCHED_IDLE;
+				else if (strstr("rr", optarg))
+   					args_sched_policy = SCHED_RR;
+				else if (strstr("qos", optarg))
+					args_sched_policy = 7;
+				else if (strstr("case", optarg))
+					args_sched_policy = 7;
+				else {
+					printf("sched policy not support\n");
+					exit(1);
+				}
+				/* getopt_long will have already printed an error */
+				break;
+	
+			case 'u':
+				arg_cpu_util = atoi(optarg);
+				if (arg_cpu_util > 100) {
+					fprintf(stderr, " cpu_util must be litter 100\n");
+					return -1;
+				}
+				break;
+
+			case 'c':
+				if (strstr("balance", optarg))
+					args_test_case = CASE_BALANCE;
+				else {
+					printf("test case not support\n");
+					exit(1);
+				}
+				break;
+
+			case 'r':
+				signal(SIGALRM, handle_alarm);
+				args_report = 1;
+				break;
+
+			case 'e':
+				args_util_increase = atoi(optarg);
+				break;
+	
+			case '?':
+				/* getopt_long will have already printed an error */
+				break;
+	
+			default:
+				/* Not sure how to get here... */
+				return -1;
+		}
+	}
+	
+	if (args_sched_policy)
+		change_sched_policy();
+
+	if (args_test_case) {
+		run_test_cases(args_test_case);
+		return 0;
+	}
+
+	if (args_thread_cnt) {
+		report_data = malloc(sizeof(unsigned long) *args_thread_cnt);
+		for (i = 0; i < args_thread_cnt; ++i) {
+			thread_data_array[i].id = i;
+			thread_data_array[i].cpu_util = arg_cpu_util + i*args_util_increase;
+			pthread_create(&thread_array[i], NULL, threadFunc, &thread_data_array[i]);
+		}
+		if (args_report)
+			alarm(1);
+		for (i = 0; i < args_thread_cnt; ++i) {
+			pthread_join(thread_array[i], NULL);
+		}
+	}
+
+	return 0;
+}
---
+++ userspace_test/sched/sched_performance/src/main.cc
@@ -0,0 +1,293 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<getopt.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<signal.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/time.h>
+#include<semaphore.h>
+#include<linux/sched.h>
+
+#define MAX_TRHEAD  (128)
+#define CHECK_MAX_CNT (1UL<<30)
+
+unsigned long args_run_ratio;
+unsigned long args_time;
+unsigned long run_cnt;
+unsigned long sleep_cnt=0;
+unsigned long thread_cnt;
+unsigned long loop_cnt;
+unsigned long msec_cnt;
+int sched_policy = 0;
+
+struct thread_data {
+	unsigned long id;
+	unsigned long run_val_ms;
+};
+
+pthread_t thread_array[MAX_TRHEAD];
+struct thread_data thread_data_array[MAX_TRHEAD];
+long long check_cnt_hz;
+unsigned long loop_cnt_hz = 1UL<<32;
+
+void hz_delay(void)
+{
+	unsigned long loop = 0;
+	while(loop++ < loop_cnt_hz);
+}
+
+void timer(int sig)
+{
+	static unsigned long old_cnt;
+	static unsigned long check_diff[5];
+	static unsigned long check_diff_off;
+
+	if(SIGALRM == sig)
+   	{
+		unsigned long diff;
+		int i;
+		diff = check_cnt_hz - old_cnt;
+		check_diff[check_diff_off++] = diff;
+		old_cnt = check_cnt_hz;
+#define MAX_CHECK_CNT (2)
+		if (check_diff_off >= MAX_CHECK_CNT) {
+			unsigned long cnt=0;
+			for (i = 0; i < MAX_CHECK_CNT; ++i)
+				cnt += check_diff[i];
+			loop_cnt_hz = cnt/(1000 * MAX_CHECK_CNT);
+			return;
+		}
+		alarm(1);       //重新继续定时1s
+	}
+	return ;
+}
+
+//get the loop cnt for HZ
+void *check_time_thread(void *param)
+{
+	unsigned long loop = 0;
+	while(check_cnt_hz++ < CHECK_MAX_CNT);
+	return NULL;
+}
+
+void *threadFunc(void *param)
+{
+	struct timeval tvafter,tvpre;
+	struct timezone tz;
+	unsigned long val = 0;
+	struct thread_data *data = (struct thread_data *)param;
+	int policy = SCHED_NORMAL;
+	struct sched_param my_params;
+	int rc, local_loop_cnt = loop_cnt;
+
+    gettimeofday (&tvpre , &tz);
+#if 0
+	//my_params.sched_priority=sched_get_priority_max(sched_policy);// 尽可能高的实时优先级
+	my_params.sched_priority=sched_get_priority_min(sched_policy);// 尽可能高的实时优先级
+
+	if (sched_policy) {
+		//rc = sched_setscheduler(0,sched_policy, &my_params);
+		rc = sched_setscheduler(0,sched_policy, &my_params);
+		printf("set sched policy:%d priority:%d\n", sched_policy, my_params.sched_priority);
+		if(rc<0)
+		{
+			//perror("sched_setscheduler to %d error", sched_policy);
+			perror("sched_setscheduler error");
+			exit(0);
+		}
+	}
+#endif
+
+	while(1)
+		sleep(1);
+	while(1) {
+		hz_delay();
+		usleep(20*args_run_ratio);
+	}
+
+    gettimeofday (&tvafter , &tz);
+	data->run_val_ms = (tvafter.tv_sec-tvpre.tv_sec)*1000+(tvafter.tv_usec-tvpre.tv_usec)/1000;
+    printf("花费时间:%ld ms\n", (tvafter.tv_sec-tvpre.tv_sec)*1000+(tvafter.tv_usec-tvpre.tv_usec)/1000);
+	return NULL;
+}
+
+void help(void)
+{
+	printf("--threads   run thread numbers\n");
+	printf("--run_cnt   run runtime count\n");
+	printf("--loop_cnt  run loop runtime count\n");
+	printf("--ratio task run ratio run:sleep \n");
+	printf("--time  task run msecond\n");
+	printf("--sleep_cnt sleep run count\n");
+	printf("--sched_policy fifo/deadline/cpuidle/qos\n");
+	printf("--sched_priority  sched priority vaule -20 - 120\n");
+}
+
+static int check_cpu_runtime(void)
+{
+	struct timeval tvafter,tvpre;
+	struct timezone tz;
+	unsigned long usec;
+
+	unsigned long cnt = 1UL<<30;
+    gettimeofday (&tvpre , &tz);
+
+	while(cnt-->0);
+    gettimeofday (&tvafter , &tz);
+
+	usec= (tvafter.tv_sec-tvpre.tv_sec)*1000+(tvafter.tv_usec-tvpre.tv_usec)/1000;
+	msec_cnt = (1UL<<30)/usec;
+    printf("花费时间:%ld ms %ld\n", usec, msec_cnt);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int choice;
+	int i, sched_priority = -1;
+	struct sched_param param;
+	int maxpri;
+	pthread_t check_thread;
+
+	thread_cnt = 1;
+	run_cnt = 1;
+	sleep_cnt = 0;
+	loop_cnt = 4;
+
+	if (argc < 2) {
+		help();
+		return 0;
+	}
+
+
+	while (1)
+	{
+		static struct option long_options[] =
+		{
+			/* Use flags like so:
+			{"verbose",	no_argument,	&verbose_flag, 'V'}*/
+			/* Argument styles: no_argument, required_argument, optional_argument */
+			{"version", no_argument,	0,	'v'},
+			{"help",	no_argument,	0,	'h'},
+			{"threads",	required_argument,	0,	'd'},
+			{"run_cnt", required_argument,	0,	'r'},
+			{"sleep_cnt", required_argument,	0,	's'},
+			{"loop_cnt", required_argument,	0,	'l'},
+			{"ratio", required_argument,	0,	'r'},
+			{"time", required_argument,	0,	't'},
+			{"sched_policy", required_argument,	0,	'p'},
+			{"sched_priority", required_argument,	0,	'i'},
+			{ 0, 0, 0, 0 }
+		};
+	
+		int option_index = 0;
+		choice = getopt_long(argc, argv, "vhd:r:s:l:p:r:t",
+					long_options, &option_index);
+	
+		if (choice == -1)
+			break;
+	
+		switch( choice )
+		{
+			case 'v':
+				break;
+	
+			case 'h':
+				help();
+				return 0;
+	
+			case 'r':
+				args_run_ratio = atoi(optarg);
+				break;
+
+			case 't':
+				args_time = atoi(optarg);
+				break;
+	
+			case 'd':
+				thread_cnt = atoi(optarg);
+				break;
+
+			case 'l':
+				loop_cnt = atoi(optarg);
+				break;
+	
+			case 's':
+				sleep_cnt = atoi(optarg);
+				break;
+	
+			case 'p':
+				if (strstr("fifo", optarg))
+					sched_policy = SCHED_FIFO;
+				else if (strstr("deadline", optarg))
+					sched_priority = SCHED_DEADLINE;
+				else if (strstr("cpuidle", optarg))
+					//sched_priority = SCHED_IDLE;
+					sched_policy = SCHED_IDLE;
+				else if (strstr("rr", optarg))
+   					sched_policy = SCHED_RR;
+				else if (strstr("qos", optarg))
+   					sched_policy = 7;
+				else {
+					printf("sched policy not support\n");
+					exit(1);
+				}
+				/* getopt_long will have already printed an error */
+				break;
+
+			case 'i':
+				sched_priority= atoi(optarg);
+				break;
+			default:
+				/* Not sure how to get here... */
+				return -1;
+		}
+	}
+	
+	//signal(SIGALRM, timer); //注册安装信号
+	//alarm(1);       //触发定时器
+
+	int policy = SCHED_NORMAL;
+	struct sched_param my_params;
+	int rc;
+	if (sched_policy) {
+		//rc = sched_setscheduler(0,sched_policy, &my_params);
+		//my_params.sched_priority=sched_get_priority_max(sched_policy);// 尽可能高的实时优先级
+		//my_params.sched_priority=20;
+		rc = sched_setscheduler(0,sched_policy, &my_params);
+		printf("set sched policy:%d priority:%d\n", sched_policy, my_params.sched_priority);
+		if(rc<0)
+		{
+			//perror("sched_setscheduler to %d error", sched_policy);
+			perror("sched_setscheduler error");
+			exit(0);
+		}
+	}
+
+	while(1) {
+		//printf("1\n");
+		sleep(1);
+		printf("1\n");
+	}
+
+	pthread_create(&check_thread, NULL, check_time_thread, NULL);
+	pthread_join(check_thread, NULL);
+
+	for (i = 0; i < thread_cnt; ++i) {
+		thread_data_array[i].id = i;
+		thread_data_array[i].run_val_ms = 0;
+		pthread_create(&thread_array[i], NULL, threadFunc, &thread_data_array[i]);
+	}
+
+	for (i = 0; i < thread_cnt; ++i) {
+		pthread_join(thread_array[i], NULL);
+	}
+
+	return 0;
+}
+
